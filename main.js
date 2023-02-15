b6df4e5ad7cb220580bc0a7bbfcd418a00c0a81b
Anaelle Surprise <anaellesurprise@Anaelles-MacBook-Air.local>
2023-02-15 14:50:49 -0500

    updates

--- main.js
+++ main.js
@@ -1,13 +1,36 @@
 // Get circle div
 
-// Set color back when mouse out
-function onClick(){
-    let circles = document.getElementById("circle")
-    console.log(circles)
-    if (circles.style.stroke == "black"){
-        circles.style.removeProperty("stroke")
+// create an event listener for all circles
+let circles = document.getElementsByClassName("circle"); 
+console.log(circles[0].getAttribute('id'))
+for(let i = 0; i<circles.length; i){
+    circles[i].addEventListener("click", displayCoords(circles[i]));
+}
+
+// when click display coordinates of last clicked circle and outline them
+function displayCoords(circle){
+    if (circle.style.stroke == "black"){
+        circle.style.removeProperty("stroke")
     }
     else{
-        circles.style.setProperty("stroke", "black");
+        circle.style.setProperty("stroke", "black");
     }
+    
+    let point = circle.getAttribute('id')
+    document.getElementById("coordinates").innerHTML = point}
+
+function coordCalc(){
+
 }
+
+// Set color back when mouse out
+function onClick(){
+    let circles = document.getElementsByClassName("circle")
+    console.log(circles)
+    console.log(circles[1])
+
+}
+//function addPoint(){}
+let xs = document.getElementById('xvalues')
+
+console.log(xs.value);
