981b62e13ef4edfa4bc10cd95738bc746003c8a8
Anaelle Surprise <anaellesurprise@Anaelles-Air.Starry-inc.net>
2023-02-15 23:30:49 -0500

    final?

--- main.js
+++ main.js
@@ -1,53 +1,51 @@
 //  CIRCLE FUNCTIONALITIES //
-// create an event listener for all circles
-let circles = document.getElementsByClassName("circle"); 
-for(let i = 0; i<circles.length; i++){
-    circles[i].addEventListener("click", onClick(circles[i]));
-}
+// When a point is added, collect necessarry parameters to add functional points
+// gets input x and y and converts them to cx and cy and converts them to x and y
 
-function onClick(circle){
-    console.log("on click ran")
-    displayCoords(circle);
-}
+let circles = document.querySelectorAll('.circle');
+console.log(circles)
 
-// when click display coordinates of last clicked circle and outline it
-function displayCoords(circle){
-    if (circle.style.stroke == "black"){
-        circle.style.removeProperty("stroke");
-    }
-    else{
-        circle.style.setProperty("stroke", "black");
-    }
-    
-    let point = circle.getAttribute('id');
-    document.getElementById("coordinates").innerHTML = point;
-    console.log("displayCoords ran");
-}
-//     ADDING NEW POINTS //
-
-// gets input x and y and converts them to cx and cy and converts them to 
 function getCoords(){
     let x = document.getElementById("xRange").value;
     let y = document.getElementById("yRange").value;
     let plotx = (50*x) +5;
     let ploty = 495 - (50*y);
-    document.querySelector('circle').insertAdjacentHTML('beforeend', addPoint(cx,cy));
+    let new_circle = addPoint(x,y,plotx,ploty)
+    document.querySelector('svg').insertAdjacentHTML('beforeend', new_circle);
+    console.log(new_circle)
+    circles = document.querySelectorAll('.circle');
+    console.log(circles)
     console.log("getCoords ran");
 }
+
+//     ADDING NEW POINTS //
 // adds HTML of a new circle
-function addPoint(cx,cy){
+function addPoint(x,y,cx,cy){
     console.log("add point ran");
-        return `<circle id= "(${x}, ${y})" cx=${cx} cy=${cy} r="5" onclick="onClick()" />`;      
+    console.log(cx, cy, x,y)
+        return `<circle class="circle" id="(${x},${y})" cx="${cx}" cy="${cy}" r="5" />`;      
 }
 
-// Set color back when mouse out
-// function onClick(){
-//     let circles = document.getElementsByClassName("circle")
-//     console.log(circles)
-//     console.log(circles[1])
+// For all items with class circle, add an event listener that checks for clicks, and performs functions on 
+// a click
+console.log(circles)
 
-// }
-//function addPoint(){}
-// let xs = document.getElementById('xvalues')
+circles.forEach(circle => {
+    circle.addEventListener('click',
+    function handleClick(event){
+        console.log("circle clicked");
+        // add stroke to circle conditional on if it has one already
+        if (circle.style.stroke == "black"){
+            circle.style.removeProperty("stroke");
+        }
+        else{
+            circle.setAttribute('style','stroke: black; stroke-width:3px');
+        }
+        // add to page the last circle clicked
+        document.getElementById("coordinates").innerHTML = circle.id;
+    });
+}
+)
 
-// console.log(xs.value);
+
+
